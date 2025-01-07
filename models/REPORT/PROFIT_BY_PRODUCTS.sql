aff4549fc2683c134b74cd67c31a61d5553e1b2f
Poornaraj <poornaraj.r@optum.com>
2025-01-07 08:48:38 +0000

    v1

--- models/REPORT/PROFIT_BY_PRODUCTS.sql
+++ models/REPORT/PROFIT_BY_PRODUCTS.sql
@@ -1,3 +1,12 @@
 SELECT
-*
+    productID, 
+    productname, 
+    category, 
+    subcategory,
+    SUM(profit)
 FROM {{ ref('STG_ORDERS') }} 
+GROUP BY 
+    productID, 
+    productname, 
+    category, 
+    subcategory
