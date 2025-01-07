a2b16e04c585087033a20873708a92e343576e13
Poornaraj <poornaraj.r@optum.com>
2025-01-07 08:40:08 +0000

    v1

---
+++ models/REPORT/PROFIT_BY_PRODUCTS.sql
@@ -0,0 +1,3 @@
+SELECT
+*
+FROM {{ ref('STG_ORDERS') }} 
--- models/STG/STG_ORDERS.sql
+++ models/STG/STG_ORDERS.sql
@@ -1,2 +1,17 @@
-select * from 
-{{ ref('RAW_ORDERS') }}
+select
+O.ORDERID,
+O.ORDERDATE,
+O.SHIPDATE,
+O.SHIPMODE,
+O.ordersellingprice - O.ordercostprice as profit,
+C.CUSTOMERID,
+C.CUSTOMERNAME,
+C.SEGMENT,
+C.COUNTRY,
+P.productID, 
+P.productName as productName,
+P.category, 
+P.subcategory
+from {{ ref('RAW_ORDERS') }} as O
+join {{ ref('RAW_CUSTOMERS') }} as C ON O.CUSTOMERID = C.CUSTOMERID
+join {{ ref('RAW_PRODUCTS') }} as P ON O.productid = P.productid
